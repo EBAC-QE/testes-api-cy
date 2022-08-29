89134ef731e1bb508f45f8525b8c3e8cbd03fb40
LariTonetto <larigottschall@hotmail.com>
2022-08-29 19:17:17 -0300

    Add configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,9 @@
-{
-    "baseUrl": "http://localhost:3000/",
-    "reporter": "mochawesome",
-    "reporterOptions": {
-        "reportDir": "mochawesome-report",
-        "overwrite": false,
-        "html": false,
-        "json": true
-    }
-}
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
